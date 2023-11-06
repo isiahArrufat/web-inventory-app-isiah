578b3f286390d844b47a4d5170cda78853aa3c3e
Isiah Arrufat <isiaharrufat@pursuit.org>
2023-11-06 01:23:02 -0500

    able to sumit and generate new aircraft however not able to runder it to the dom

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 const form = document.getElementById("new-Aircraft");
+const ul = document.createElement("ul");
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
@@ -8,10 +9,26 @@ form.addEventListener("submit", (event) => {
    console.log("aircraftType:", event.target.aircraftType.value);
    console.log("machSpeed:", event.target.machSpeed.value);
 
+    const {name, price, inStock, aircraftType, machSpeed} = event.target;
+  
+    AirCraftTemplate(name.value, price.value, inStock.value, aircraftType.value, machSpeed.value);
 })
 
-const addAirCraft = (name, ) => {
 
+//  Helper function to submitted a new aricraft 
+const AirCraftTemplate = (name, price, inStock, aircraftType, machSpeed) => {
+    const li = document.createElement("li");
+    li.classList.add("newAirCraft");
+    
+    if (name && price && inStock && aircraftType && machSpeed) {
+        li.innerHTML = `<img src="" alt ="Pic Of Aircraft"><p>
+        <strong>Aircraft Name</strong>: ${name}</p><strong>Price Of 
+        Aircraft</strong>: ${price}</p><p><strong>Available??</strong>: 
+        ${inStock}</p><p><strong>Type Of AirCraft</strong>: ${aircraftType}
+        </p><p><strong>Aircraft TopSpeed</strong>: ${machSpeed}`;
+    }
+    
+    ul.append(li);
 }
 
 
