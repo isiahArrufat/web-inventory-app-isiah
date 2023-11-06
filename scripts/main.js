6f78c126d687eebbf29eb9c5fbebced09ce7c271
Isiah Arrufat <isiaharrufat@pursuit.org>
2023-11-06 00:36:36 -0500

    found inputs to be used for Aircraft template

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,21 @@
+const form = document.getElementById("new-Aircraft");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+   console.log("name:", event.target.name.value);
+   console.log("price:", event.target.price.value);
+   console.log("inStock:", event.target.inStock.value);
+   console.log("aircraftType:", event.target.aircraftType.value);
+   console.log("machSpeed:", event.target.machSpeed.value);
+
+})
+
+const addAirCraft = (name, ) => {
+
+}
+
+
+
 
 
 
